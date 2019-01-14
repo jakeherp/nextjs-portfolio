cd1921e7ee7ba8979f70a83c155a5cfc48592c3e
Jacob Herper <jacob@herper.uk>
2019-01-14 23:08:55 +0000

    Portfolio improvements and bug fixes

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2bace47e1c1f33a04f01";
+/******/ 	var hotCurrentHash = "098b45f24ed0de97e41e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -21,44 +21,42 @@ class Portfolio extends React.Component {
 
     try {
       const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
+        "https://www.behance.net/v2/users/jakeherp/projects/?api_key=Hvz37g2VCwNq7NLdPSdr4ITJOSN7QyAz"
       );
-      posts = response.data;
+      posts = response.data.projects;
     } catch (err) {
       console.error(err);
     }
 
     return {
-      posts: posts.splice(0, 10)
+      posts: posts.splice(0, 9)
     };
   }
 
   renderPosts(posts) {
-    return posts.map((post, index) => {
-      return (
-        <Col md="4">
-          <React.Fragment key={index}>
-            <span>
-              <Card className="portfolio-card">
-                <CardHeader className="portfolio-card-header">
-                  Some Position {index}
-                </CardHeader>
-                <CardBody>
-                  <p className="portfolio-card-city"> Some Location {index} </p>
-                  <CardTitle className="portfolio-card-title">
-                    Some Company {index}
-                  </CardTitle>
-                  <CardText className="portfolio-card-text">
-                    Some Description {index}
-                  </CardText>
-                  <div className="readMore"> </div>
-                </CardBody>
-              </Card>
-            </span>
-          </React.Fragment>
-        </Col>
-      );
-    });
+    return posts.map((post, index) => (
+      <Col md="4" key={index}>
+        <React.Fragment>
+          <span>
+            <Card className="portfolio-card">
+              <CardHeader className="portfolio-card-header">
+                {post.name}
+              </CardHeader>
+              <CardBody>
+                <p className="portfolio-card-city"> Some Location {index} </p>
+                <CardTitle className="portfolio-card-title">
+                  Some Company {index}
+                </CardTitle>
+                <CardText className="portfolio-card-text">
+                  Some Description {index}
+                </CardText>
+                <div className="readMore"> </div>
+              </CardBody>
+            </Card>
+          </span>
+        </React.Fragment>
+      </Col>
+    ));
   }
 
   render() {
